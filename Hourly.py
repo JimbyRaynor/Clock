a60fa860e8203fcb6f246a37be0f32f7f4cb8d12
Jimby Raynor <67350304+JimbyRaynor@users.noreply.github.com>
2025-08-10 13:48:06 +1000

    Add files via upload

--- Hourly.py
+++ Hourly.py
@@ -17,55 +17,63 @@ fonttiny = ("Arial",18)
 currentDate = datetime.datetime.now()
 DayofWeek = currentDate.strftime("%a")
 
-def setAfternoon():
-    textbox1.delete("1.0","end-1c")
-    if DayofWeek == "Mon":
-       textbox1.insert(INSERT,"Clean Microwave\n")
-    textbox1.insert(INSERT,"Open window 2 PM\n")
-    textbox1.insert(INSERT,"Backup to Github\n")
-    if DayofWeek == "Sat":
-       textbox1.insert(INSERT,"9AM Google meet call\n")
-       textbox1.insert(INSERT,"Backup Steam Deck to USB\n")
-       textbox1.insert(INSERT,"Clean Showers\n")
-       textbox1.insert(INSERT,"Empty tissue bins\n")
-       textbox1.insert(INSERT,"Sanding\n")
-       textbox1.insert(INSERT,"Nails\n")
-       textbox1.insert(INSERT,"Nose/Ears\n")
-       textbox1.insert(INSERT,"Vacuuming\n")
-       textbox1.insert(INSERT,"Empty Recycling Bin\n")
-       textbox1.insert(INSERT,"Check plants around gas hot water heater\n")
-       textbox1.insert(INSERT,"Check garage roof\n")
-       textbox1.insert(INSERT,"Add sunrise/sunset dates to clock\n")
-       textbox1.insert(INSERT,"Tidy letters\n")
-       textbox1.insert(INSERT,"Trim grass edges\n")
-       textbox1.insert(INSERT,"Clear roof spouting\n")
-    if DayofWeek == "Tue":
-       textbox1.insert(INSERT,"Vacuuming\n")
-       textbox1.insert(INSERT,"Clean Floor\n")
-    if DayofWeek == "Sun" or DayofWeek == "Tue" or DayofWeek == "Thu":
-       textbox1.insert(INSERT,"Google Call\n")
-       textbox1.insert(INSERT,"Distribute Exercise Sheets\n")
-    if DayofWeek == "Sun":
-       textbox1.insert(INSERT,"Advertise Computing Lecture\n")
-       textbox1.insert(INSERT,"Vacuum behind heaters\n")
-       textbox1.insert(INSERT,"Put Rubbish Out\n")
-    if DayofWeek == "Thu":
-       textbox1.insert(INSERT,"Clean solar panels\n")
+Notes = []
 
-textbox1 = Text(mainwin,width=26,height=8,font=fonttiny,bg="black",fg="orange")
-textbox1.place(x=0,y=0)
+def add(mystring):
+   Notes.append(mystring.upper())
+
+def addnotes():
+    if DayofWeek == "Fri":
+       add("Clean Microwave")
+    add("Open window 2 PM")
+    add("Backup to Github")
+    if DayofWeek == "Sat":
+       add("9AM Google meet call\n")
+       add("Backup Steam Deck to USB\n")
+       add("Clean Showers\n")
+       add("Wash Showerhead\n")
+       add("Empty tissue bins\n")
+       add("Nose/Ears\n")
+       add("Vacuuming\n")
+       add("Empty Recycling Bin\n")
+       add("Check garage roof\n")
+       add("Add sunrise/sunset dates to clock\n")
+       add("Clear roof spouting\n")
+    if DayofWeek == "Tue":
+       add("Vacuuming\n")
+       add("Clean Floor\n")
+    if DayofWeek == "Sun" or DayofWeek == "Tue" or DayofWeek == "Thu":
+       add("Google Call\n")
+       add("Distribute Exercise Sheets\n")
+    if DayofWeek == "Sun":
+       add("Advertise Computing Lecture\n")
+       add("Vacuum behind heaters\n")
+       add("Put Rubbish Out\n")
+    if DayofWeek == "Thu":
+       add("Clean solar panels\n")
+
+
 canvas1 = Canvas(mainwin,width=220,height=330,bg="black")
 canvas1.place(x=380,y=0)
 
-setAfternoon()
+canvasReminder = Canvas(mainwin,width=379,height=330,bg="black")
+canvasReminder.place(x=0,y=0)
 
 CPUusage = LEDlib.LEDtextobj(canvas1,x=0,y=14,text="CPU",colour="light green", pixelsize =3, charwidth=22)
 CPUusage2 = LEDlib.LEDtextobj(canvas1,x=70,y=7,text="%",colour="yellow", pixelsize =4, charwidth=29)
+
+addnotes()
 
 ProcList = []
 for i in range(14):
   Proc = LEDlib.LEDtextobj(canvas1,x=0,y=44+20*i,text=" ",colour="light blue", pixelsize =2, charwidth=14, solid = True)
   ProcList.append(Proc)
+
+
+NoteList = []
+for i in range(min(14,len(Notes))):
+  note = LEDlib.LEDtextobj(canvasReminder,x=0,y=4+23*i,text=Notes[i],colour="light green", pixelsize =2, charwidth=14, solid = False)
+  NoteList.append(note)
 
 def resetProcs():
     for i in range(14):
@@ -85,8 +93,22 @@ def updateCpuUsage():
 def getTopProc(n=4):
    processes.clear()
    for proc in psutil.process_iter(['pid', 'name', 'cpu_percent']):
-      if proc.info['cpu_percent'] > 0.4:
-         processes.append(proc.info['name']+": "+str(proc.info['cpu_percent'])+"%")   
+      name = " "+proc.info['name']
+      cpu = proc.info['cpu_percent'] 
+      python = False
+      kworker = False
+      code = False
+      if name.find("kworker") >= 0: 
+         kworker = True
+      if name.find("python") >= 0: 
+         python = True
+         processes.append(name+": "+str(proc.info['cpu_percent'])+"%")
+      if name.find("code") >= 0 and cpu > 0.4: 
+         python = True
+         processes.append(name+": "+str(proc.info['cpu_percent'])+"%")
+      if cpu > 0.9 and not python and not kworker and not code:
+         processes.append(name[1:10]+": "+str(proc.info['cpu_percent'])+"%") 
+   processes.sort()  
 
 updateCpuUsage()
 
